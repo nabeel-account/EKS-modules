fd8f40d3fff9ebae2c4c4e9ce93ba8688012b623
Nabeel Hamad <n.hamad@hotmail.co.uk>
2024-04-04 16:14:07 +0100

    add section on retriving ArgoCD admin password

--- 4-argocd.tf
+++ 4-argocd.tf
@@ -14,3 +14,17 @@ resource "helm_release" "argocd" {
     templatefile("${path.module}/argocd/argocd-values.yaml" , {})
   ]
 }
+
+resource "null_resource" "password" {
+  provisioner "local-exec" {
+    working_dir = "./argocd"
+    command     = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
+  }
+}
+
+# resource "null_resource" "del-argo-pass" {
+#   depends_on = [null_resource.password]
+#   provisioner "local-exec" {
+#     command = "kubectl -n argocd delete secret argocd-initial-admin-secret"
+#   }
+# }
